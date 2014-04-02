c172b5fb8d3753294fd23e5d66829ca4acc807b7
lenny <lenny@localhost>
2014-04-01 22:27:47 -0700

    .

--- CCBasics/src/com/cfdigital/cc/ccbasics/CCBasics.java
+++ CCBasics/src/com/cfdigital/cc/ccbasics/CCBasics.java
@@ -2,12 +2,14 @@ package com.cfdigital.cc.ccbasics;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.cfdigital.cc.ccbasics.commands.Ban;
+
 public class CCBasics extends JavaPlugin {
 	
 	
 	@Override
 	public void onEnable() {
-		getCommand("Ban").setExecutor(new Ban(this));
+		getCommand("Ban").setExecutor(new Ban());
 
 	}
 }
